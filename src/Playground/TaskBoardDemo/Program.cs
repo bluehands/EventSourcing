4107e59b68003c78262d5116e0d268268daeb329
Alexander Wiedemann <wiedemann@bluehands.de>
2024-02-05 09:27:05 +0100

    - after first session

--- src/Playground/TaskBoardDemo/Program.cs
+++ src/Playground/TaskBoardDemo/Program.cs
@@ -1,10 +1,63 @@
-﻿namespace TaskBoardDemo
+﻿using EventSourcing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace TaskBoardDemo;
+
+class Program
 {
-    class Program
+    static async Task Main()
     {
-        static void Main()
-        {
-            
-        }
+        using var host = Host.CreateDefaultBuilder()
+            .ConfigureServices(serviceCollection =>
+            {
+                serviceCollection
+                    .AddEventSourcing(options => options.UseSqliteEventStore(@"Data Source=c:\temp\EventStore.db"));
+                serviceCollection.AddSingleton<Tester>();
+
+            })
+            .ConfigureLogging(l => l.AddConsole())
+            .Build();
+
+        await host.Services.StartEventSourcing();
+
+        await host.StartAsync();
+
+        var tester = host.Services.GetRequiredService<Tester>();
+        await tester.Run();
+
+        await host.WaitForShutdownAsync();
     }
 }
+
+public class Tester(IEventStore store, IObservable<Event> eventStream)
+{
+    public async Task Run()
+    {
+        eventStream
+            .Subscribe(e => Console.WriteLine($"Received event: {e.Position}: {e.Payload}"));
+
+        //var allEvents = await store.ReadEvents(0).ToListAsync();
+
+        //foreach (var @event in allEvents)
+        //{
+        //    Console.WriteLine($"Read event: {@event.Position}: {@event.Payload}");
+        //}
+
+        await store.WriteEvents([new TaskAdded("MyTask", "Trifft sich vorbereiten")]);
+    }
+}
+
+public static class EventTypes
+{
+    public const string TaskAdded = "TaskAdded";
+}
+
+public static class StreamIds
+{
+    public static readonly StreamId Task = new("Tasks", "Tasks");
+}
+
+
+public record TaskAdded(string Name, string Title) : EventPayload(StreamIds.Task, EventTypes.TaskAdded);
---
+++ src/Playground/TaskBoardDemo/SerializablePayloads/TaskAdded.cs
@@ -0,0 +1,13 @@
+﻿using EventSourcing;
+
+namespace TaskBoardDemo.SerializablePayloads;
+
+[SerializableEventPayload(EventTypes.TaskAdded)]
+public record TaskAdded(string Name, string? Title, string? Abstract);
+
+public class TaskAddedPayloadMapper : EventPayloadMapper<TaskBoardDemo.TaskAdded, TaskAdded>
+{
+    protected override TaskBoardDemo.TaskAdded MapFromSerializablePayload(TaskAdded serialized, StreamId streamId) => new (serialized.Name, serialized.Title ?? serialized.Abstract!);
+
+    protected override TaskAdded MapToSerializablePayload(TaskBoardDemo.TaskAdded payload) => new (payload.Name, payload.Title, null);
+}
