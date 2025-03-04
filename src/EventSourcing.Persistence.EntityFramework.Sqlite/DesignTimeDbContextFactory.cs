959ebd3b79cecf289f8ef38e24fa3b831fa5eddd
Jasper Park <jasper.park@bluehands.de>
2025-03-04 13:53:20 +0100

    feat: set the journal mode of sqlite databases to WAL via migration

---
+++ src/EventSourcing.Persistence.EntityFramework.Sqlite/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EventSourcing.Persistence.EntityFramework.Sqlite;
+
+internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EventStoreContext>
+{
+    public EventStoreContext CreateDbContext(string[] args)
+    {
+        var connectionString = $"DataSource=MigrationsDb.db";
+        var options = new DbContextOptionsBuilder<EventStoreContext>()
+            .UseSqlite(connectionString, sqlite => sqlite.MigrationsAssembly(typeof(DesignTimeDbContextFactory).Assembly.FullName))
+            .Options;
+        return new EventStoreContext(options);
+    }
+}
