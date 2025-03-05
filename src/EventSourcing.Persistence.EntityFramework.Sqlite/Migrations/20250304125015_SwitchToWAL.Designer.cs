461b0997643b878695b8f54f307d9bd185223396
Jasper Park <145050804+park-jasper@users.noreply.github.com>
2025-03-05 07:52:24 +0100

    feat: set the journal mode of sqlite databases to WAL via migration (#8)

--- src/EventSourcing.Persistence.EntityFramework.Sqlite/Migrations/20250304125015_SwitchToWAL.Designer.cs
+++ src/EventSourcing.Persistence.EntityFramework.Sqlite/Migrations/20250304125015_SwitchToWAL.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using EventSourcing.Persistence.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventSourcing.Persistence.EntityFramework.Sqlite.Migrations
 {
     [DbContext(typeof(EventStoreContext))]
-    partial class EventStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20250304125015_SwitchToWAL")]
+    partial class SwitchToWAL
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.11");
---
+++ src/EventSourcing.Persistence.EntityFramework.Sqlite/Migrations/20250304125015_SwitchToWAL.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventSourcing.Persistence.EntityFramework.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwitchToWAL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                PRAGMA journal_mode=wal;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
